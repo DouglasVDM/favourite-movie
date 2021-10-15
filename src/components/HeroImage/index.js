02dd256cab6e0854a0a046c88d2b52bd4426913a
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-10-15 22:15:28 +0200

    create hero image section

---
+++ src/components/HeroImage/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// STYLES
+import { Wrapper, Content, Text } from "./HeroImage.styles";
+
+const HeroImage = (prop) => {
+      <Wrapper image={prop.image} />
+}
+
+export default HeroImage
