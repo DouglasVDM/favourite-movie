07c74e61cae7fe635f18ead496c76d50064e51c8
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-10-16 11:38:20 +0200

    create search bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,12 +11,13 @@ import HeroImage from './HeroImage';
 import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner'
+import SearchBar from './SearchBar';
 
 // IMAGE
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-  const { state, loading, error } = useHomeFetch();
+  const { state, loading, error, setSearchTerm } = useHomeFetch();
 
   console.log('state:', state);
 
@@ -29,6 +30,7 @@ const Home = () => {
           text={state.results[0].overview}
         />
         : null}
+      <SearchBar setSearchTerm={setSearchTerm} />
       <Grid header={'Popular Movies'}>
         {state.results.map((movie) => (
           <Thumb
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -7,13 +7,16 @@ const initialState = {
   page: 0,
   results: [],
   total_pages: 0,
-  total_results:0
+  total_results: 0
 }
 
 export const useHomeFetch = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   const [state, setState] = useState(initialState);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  console.log('searchTerm:', searchTerm);
 
   const fetchMovies = async (page, searchTerm = '') => {
     try {
@@ -40,5 +43,5 @@ export const useHomeFetch = () => {
     fetchMovies(1)
   }, []);
 
-  return { state, loading, error };
+  return { state, loading, error, setSearchTerm };
 }
