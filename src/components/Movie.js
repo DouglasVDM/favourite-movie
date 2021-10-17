647ed8af533317dad2f53bd24544b2ccf21c495c
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-10-17 20:10:05 +0200

    components for movie page breadcrumbs

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -13,17 +13,21 @@ import { useMovieFetch } from '../hooks/useMovieFetch';
 
 // IMAGE
 import NoImage from '../images/no_image.jpg';
+import BreadCrumb from './BreadCrumb';
 
 const Movie = () => {
   const { movieId } = useParams();
   const { state: movie, loading, error } = useMovieFetch(movieId);
 
-  console.log('movie:',movie)
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong...</div>
+
+  console.log('movie:', movie)
   return (
     <>
-      Movie
+      <BreadCrumb movieTitle={movie.original_title} />
     </>
-  )
-}
+  );
+};
 
 export default Movie;
