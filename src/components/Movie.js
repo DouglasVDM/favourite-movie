a87b23bb34cd211ff80237f638b280b750d2b1a2
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-10-17 19:28:15 +0200

    fetch data for movie with custom hook

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 // CONFIG
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
@@ -8,11 +9,16 @@ import Grid from './Grid';
 import Spinner from './Spinner';
 
 // HOOK
+import { useMovieFetch } from '../hooks/useMovieFetch';
 
 // IMAGE
 import NoImage from '../images/no_image.jpg';
 
 const Movie = () => {
+  const { movieId } = useParams();
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+
+  console.log('movie:',movie)
   return (
     <>
       Movie
---
+++ src/hooks/useMovieFetch.js
@@ -0,0 +1,57 @@
+import { useCallback, useEffect, useState } from "react";
+
+// API
+import API from '../API';
+
+const initialState = {
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0
+}
+
+export const useMovieFetch = (movieId) => {
+  const [state, setState] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  console.log('state:', state);
+
+  // 
+  const fetchMovie = useCallback(async () => {
+    try {
+      setLoading(true);
+      setError(false);
+
+      const movie = await API.fetchMovie(movieId);
+      console.log('movie:', movie)
+
+      const credits = await API.fetchCredits(movieId);
+      console.log('credits:', credits)
+
+      // GET DIRECTORS ONLY
+      const directors = credits.crew.filter(
+        member => member.job === 'Director'
+      );
+
+      setState({
+        ...movie,
+        actors: credits.cast,
+        directors
+      });
+
+      setLoading(false)
+    } catch (error) {
+      console.error(error.message)
+      setError(true);
+    }
+  }, [movieId]);
+
+  // INITIAL RENDER
+  useEffect(() => {
+    fetchMovie();
+  }, [movieId, fetchMovie]);
+
+
+  return { state, loading, error };
+}
