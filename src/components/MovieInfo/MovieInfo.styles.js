3ce0bfb8e1d402f1fc4ad060d03fc29be13dd723
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-10-17 21:47:17 +0200

    style component for movie info

--- src/components/MovieInfo/MovieInfo.styles.js
+++ src/components/MovieInfo/MovieInfo.styles.js
@@ -3,32 +3,72 @@ import styled from "styled-components";
 import { IMAGE_BASE_URL, BACKDROP_SIZE } from "../../config";
 
 export const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 70px;
-  background: var(--medGrey);
-  color: var(--white);
+  background: ${({ backdrop }) =>
+    backdrop ? `url(${IMAGE_BASE_URL}${BACKDROP_SIZE}${backdrop})` : '#000'};
+  background-size: cover;
+  background-position: center;
+  padding: 40px 20px;
+  animation: animateMovieInfo 1s;
+
+  @keyframes animateMovieInfo {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  
 `;
 
 export const Content = styled.div`
   display: flex;
-  width: 100%;
   max-width: var(--maxWidth);
-  padding: 0 20px;
-
-  span {
-    font-size: var(--fontMed);
-    color: var(--white);
-    padding-right: 10px;
+  margin: 0 auto;
+  background: rgba(0,0,0,0.7);
+  border-radius: 20px;
 
     @media screen and (max-width: 768px) {
-      font-size: var(--fontSmall);
+      display: block;
+      max-height: none;
     }
-  }
 `;
 
 export const Text = styled.div`
-  
+  width: 100%;
+  padding: 20px 40px;
+  color: var(--white);
+  overflow: hidden;
+
+  .rating-directors {
+    display: flex;
+    justify-content: flex-start;    
+  }
+
+  .score {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 35px;
+    height: 35px;
+    background: #fff;
+    color: #000;
+    font-weight: 800;
+    border-radius: 50%;
+    margin: 0;
+  }
+
+  .director {
+    margin: 0 0 0 40px;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  h1 {
+    @media screen and (max-width: 768px) {
+      font-size: var(--fontBig);
+    }
+  }
 `;
