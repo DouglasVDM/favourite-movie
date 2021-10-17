62a15397d2a2f68d90ae3fcca1df39523a025d74
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-10-17 18:49:17 +0200

    create movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+
+// CONFIG
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+
+// COMPONENTS
+
+// HOOK
+
+// IMAGE
+import NoImage from '../images/no_image.jpg';
 
 const Movie = () => {
   return (
-    <div>
+    <>
       Movie
-    </div>
+    </>
   )
 }
 
-export default Movie
+export default Movie;
