7aae4e61de6b92b11bb02f78b31960763f58d8a5
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-10-31 22:05:35 +0200

    add semicolon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // ROUTING
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // COMPONENTS
 import Header from './components/Header';
